0a5168eff3bc7ec17549c80f0a24e7c95f458ffc
Brooks Patton <brooks@brookzerker.com>
2016-09-05 16:12:42 -0600

    a user can now create todo items, but the input has been sanitized to not allow over 255 characters

--- api/index.js
+++ api/index.js
@@ -4,6 +4,22 @@ const express = require('express');
 let router = express.Router();
 const pg = require('../db/knex_config.js');
 
+router.post('/v1/items', (req, res, next) => {
+  // if the name is greate than 255 characters, then don't do anything
+  let name = req.body.name;
+  let description = req.body.description;
+
+  if(name.length > 255) {
+    return res.redirect('/');
+  }
+
+  if(description.length > 255) {
+    return res.redirect('/');
+  }
+
+  next();
+});
+
 router.post('/v1/items', (req, res, next) =>{
   pg('todo').insert(req.body)
     .then(() => {
